dfed03fdb04385ce5784c7cdcbbe90bb5c810de9
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-11-28 11:50:44 +0200

    crreating multiple variables and assigning them to a single value.

--- w3schools/Main.java
+++ w3schools/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 public static void main(String[] args){
 
-String final blue = "BLUE";
-String final red = "RED";
+String blue, red, violet;
+violet =  blue = red = " Color";
 
-    System.out.print("I got "+ red + " I got " + blue + " What you WAANT!!");
+    System.out.print(blue  + red);
 }
 }
