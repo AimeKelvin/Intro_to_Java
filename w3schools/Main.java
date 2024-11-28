37df485c65d98eb89fce02decca4e0ec4d461380
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-11-28 11:27:12 +0200

    Created my first ever Java file
    Created my First ever Java file and  ran my first ever Java code.

---
+++ w3schools/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String[] args){
+
+String final blue = "BLUE";
+String final red = "RED";
+
+    System.out.print("I got "+ red + " I got " + blue + " What you WAANT!!");
+}
+}
