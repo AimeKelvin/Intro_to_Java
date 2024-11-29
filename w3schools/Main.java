4794e8498342e6a70ed21bebcab7d785463720c3
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-11-29 13:36:39 +0200

    program to calculate input n output in java

--- w3schools/Main.java
+++ w3schools/Main.java
@@ -1,8 +1,15 @@
 public class Main {
 public static void main(String[] args){
 
-int length = 4;
-int width = 3;
-int area = length * width //Formula of area of rectangle is L X W
+int items = 50;
+float costPerItems = 9.90f;
+float totalCost = items * costPerItems;
+String currency = "Frw";
+
+//Print variables 
+System.out.println("Number of items: " + items);
+System.out.println("Cost per item: "+ costPerItems + currency);
+System.out.println("Total cost = " + totalCost + currency);
+ 
 }
 }
