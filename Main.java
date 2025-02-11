c767921d452a32be5941e88a3bb04860c1168ace
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-02-11 14:55:03 +0200

    playing around with scanners

--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-System.out.print("I like Sausage!");
+        //create Variable scanner
+    Scanner scanner = new Scanner(System.in);
+
+    int age;
+
+System.out.println("Whats your age: ");
+age= scanner.nextInt();
+
+     if(age < 18){
+        System.out.println("You are a kid lil bro");
+     }else if(age >= 18){
+        System.out.println("Jojo POSE POSE POSE.");
+     }
+
+    
+
     }
 }
